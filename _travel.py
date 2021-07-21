ce8debdb0c26c3b8d08dc4e5ce110280405f7d81
JamesBear <jamesgrizzly@gmail.com>
2021-07-21 11:55:37 +0800

    command to restart kuma(for reloading config)

--- _travel.py
+++ _travel.py
@@ -1356,6 +1356,15 @@ class Travel(QWidget):
             return self.screen.close(args)
         elif key == 'quit':
             sys.exit(0)
+        elif key == 'restart':
+            exe = sys.executable
+            if ' ' in exe:
+                exe = '"' + exe + '"'
+
+            # wow, this command is amazing, no longer needs to sys.exit
+            os.execv(exe, [exe, sys.argv[0]] + sys.argv)
+            #sys.exit(0)
+
         elif key == 'demo':
             self.find_and_insert_code_snippet(args)
             return 'destroy'
