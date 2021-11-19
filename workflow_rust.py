c8d05907f25a437eac0f03425812f11a4779d7b1
JamesBear <jamesgrizzly@gmail.com>
2021-11-19 15:44:08 +0800

    reserve cwd as kuma

--- workflow_rust.py
+++ workflow_rust.py
@@ -1,10 +1,15 @@
 from base import Message
 from rust_eval import rust_eval
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def main(kuma, args):
     try:
         result = rust_eval(args)
+        os.chdir(BASE_DIR)
     except Exception as e:
+        os.chdir(BASE_DIR)
         return Message('Error: {}'.format(e))
     print(result)
     return Message('= '.format(result), 5000)
