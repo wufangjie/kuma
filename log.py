a7c022aa35d861e4bcaa02a5919e497aa8f23f89
wfj <wufangjie1223@126.com>
2022-04-12 19:33:00 +0800

    add [%(filename)s:%(lineno)s] since multiple files will use log

--- log.py
+++ log.py
@@ -10,20 +10,20 @@ LOG_FILE = 'debug.log'
 _initialized = False
 
 def init_log():
+    fmt_str = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(filename)s:%(lineno)s] %(message)s"
     global _initialized
     if not _initialized:
         _initialized = True
         logging.basicConfig(
             level=logging.INFO,
-            format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+            format=fmt_str,
             handlers=[
                 #logging.FileHandler("{0}/{1}.log".format(logPath, fileName)),
                 #logging.StreamHandler(sys.stdout)
             ])
 
-                
         logging_file_path = os.path.join(BASE_DIR, LOG_FILE)
-        log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+        log_formatter = logging.Formatter(fmt_str)
 
         #file_handler = RotatingFileHandler(logging_file_path, mode='a', maxBytes=150*1024*1024, backupCount=2, encoding='utf-8')
         file_handler = logging.FileHandler(logging_file_path, mode='a', encoding='utf-8')
